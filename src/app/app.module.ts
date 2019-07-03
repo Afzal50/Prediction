18dbd6bb7e47ed911dd2f904166a5ff403b6d77a
Ghori Afzal <ghoriafzal50@gmail.com>
2019-07-04 01:16:02 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StocksComponent } from './stocks/stocks.component';
-import { PredictServiceService } from './predict-service.servie/PredictServiceService'; 
+import { PredictServiceService } from './predict-service.servie/PredictServiceService';
 @NgModule({
   declarations: [
     AppComponent,
