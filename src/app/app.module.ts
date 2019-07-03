b0cbf5d8d80e8c6c240ca355d1889d6635559183
Ghori Afzal <ghoriafzal50@gmail.com>
2019-07-03 22:59:22 +0800

    Angular Project
    Prediction

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { PredictServiceService } from './predict-service.servie/PredictServiceService'; 
+@NgModule({
+  declarations: [
+    AppComponent,
+    StocksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/stocks/stocks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { PredictServiceService } from '../predict-service.service/PredictServiceService';
+@Component({
+  selector: 'app-stocks',
+  templateUrl: './stocks.component.html',
+  styleUrls: ['./stocks.component.css']
+})
+export class StocksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
